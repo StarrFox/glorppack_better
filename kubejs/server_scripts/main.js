f23872a69b8a3aaed5dbff1213c027caf7710c6c
StarrFox <StarrFox6312@gmail.com>
2026-05-03 07:38:19 -0500

    add kelp recipe

---
+++ kubejs/server_scripts/main.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(event => {
+    event.recipes.ars_nouveau.imbuement (
+        "#minecraft:saplings",
+        "minecraft:kelp",
+        0,
+        ["minecraft:water_bucket"]
+    )
+})
